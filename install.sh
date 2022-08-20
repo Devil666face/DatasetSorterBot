000682facbe2ccf8b472fae26d01eb36932d3bee
Devil666face <artem1999k@gmail.com>
2022-08-20 21:06:26 +0300

    release

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Укажите путь к текущей директории. Пример /home/king/pythonProject/DatasetSorterBot/
-read dir_name
-cd $dir_name
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
