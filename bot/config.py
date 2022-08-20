d800e0f43632e5dc03f7cd828cd16617e1dd848f
Devil666face <artem1999k@gmail.com>
2022-08-20 21:22:30 +0300

    make compile version

---
+++ bot/config.py
@@ -0,0 +1,4 @@
+TOKEN = '5273448805:AAF15e6Js3bQlz5fI_WMe0HCa4UpZkQ6WBQ'
+FIRST_FOLDER = '/home/king/pythonProject/MilitaryReco/dataset/military/'
+SECOND_FOLDER = '/home/king/pythonProject/MilitaryReco/dataset/'
+#ПУТИ К ПАПКАМ С ФОТО КОНЧАЮТСЯ НА /
